f763d7d2288c02824049310eb4adb035132f8c7f
xiaoyichao <xiao_yi_chao@163.com>
2023-05-04 22:49:12 +0800

    test

--- test.py
+++ test.py
@@ -11,6 +11,10 @@ import sys
 import torch
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
 from torch.utils.data.distributed import DistributedSampler
+import transformers
+from modeling_chatglm import ChatGLMForConditionalGeneration
+from tokenization_chatglm import ChatGLMTokenizer
+from data_set_sft import SFTDataSet, coll_fn
 
 from transformers import (
     AutoModelForCausalLM,
@@ -18,13 +22,14 @@ from transformers import (
     default_data_collator,
     get_scheduler,
 )
-
+from peft import LoraConfig, get_peft_model, get_peft_model_state_dict, prepare_model_for_int8_training, \
+    set_peft_model_state_dict
 import deepspeed
 from deepspeed.ops.adam import DeepSpeedCPUAdam, FusedAdam
 
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
-# from utils.data.data_utils import create_prompt_dataset
+from utils.data.data_utils import create_prompt_dataset
 from utils.utils import print_rank_0, to_device, save_hf_format, set_random_seed, get_all_reduce_mean, get_optimizer_grouped_parameters, save_zero_three_model, load_hf_tokenizer
 from utils.ds_utils import get_train_ds_config
 # from utils.module.lora import convert_linear_layer_to_lora, convert_lora_to_linear_layer, only_optimize_lora_parameters
@@ -35,12 +40,12 @@ def parse_args():
     parser = argparse.ArgumentParser(
         description=
         "Finetune a transformers model on a causal language modeling task")
-    parser.add_argument('--data_path',
-                        nargs='*',
-                        default=['Dahoas/rm-static'],
-                        help='Path to the training dataset. Accepted format:'
-                        '1) a single data path, 2) multiple datasets in the'
-                        'form: dataset1-path dataset2-path ...')
+    # parser.add_argument('--data_path',
+    #                     nargs='*',
+    #                     default=['Dahoas/rm-static'],
+    #                     help='Path to the training dataset. Accepted format:'
+    #                     '1) a single data path, 2) multiple datasets in the'
+    #                     'form: dataset1-path dataset2-path ...')
     parser.add_argument('--data_split',
                         type=str,
                         default='2,4,4',
@@ -96,17 +101,17 @@ def parse_args():
                         type=float,
                         default=0.,
                         help="Weight decay to use.")
-    parser.add_argument("--num_train_epochs",
-                        type=int,
-                        default=1,
-                        help="Total number of training epochs to perform.")
-    parser.add_argument(
-        "--gradient_accumulation_steps",
-        type=int,
-        default=1,
-        help=
-        "Number of updates steps to accumulate before performing a backward/update pass.",
-    )
+    # parser.add_argument("--num_train_epochs",
+    #                     type=int,
+    #                     default=1,
+    #                     help="Total number of training epochs to perform.")
+    # parser.add_argument(
+    #     "--gradient_accumulation_steps",
+    #     type=int,
+    #     default=1,
+    #     help=
+    #     "Number of updates steps to accumulate before performing a backward/update pass.",
+    # )
     parser.add_argument(
         "--lr_scheduler_type",
         type=SchedulerType,
@@ -122,18 +127,18 @@ def parse_args():
         type=int,
         default=0,
         help="Number of steps for the warmup in the lr scheduler.")
-    parser.add_argument("--output_dir",
-                        type=str,
-                        default=None,
-                        help="Where to store the model.")
+    # parser.add_argument("--output_dir",
+    #                     type=str,
+    #                     default=None,
+    #                     help="Where to store the model.")
     parser.add_argument("--seed",
                         type=int,
                         default=1234,
                         help="A seed for reproducible training.")
-    parser.add_argument("--local_rank",
-                        type=int,
-                        default=-1,
-                        help="local_rank for distributed training on gpus")
+    # parser.add_argument("--local_rank",
+    #                     type=int,
+    #                     default=-1,
+    #                     help="local_rank for distributed training on gpus")
     parser.add_argument('--gradient_checkpointing',
                         action='store_true',
                         help='Enable HF gradient checkpointing for model.')
@@ -161,6 +166,21 @@ def parse_args():
     parser.add_argument('--only_optimize_lora',
                         action='store_true',
                         help='Only optimize the LoRA parameters.')
+
+    # 自己添加的参数
+    parser.add_argument('--train_path', default='/app/ChatGLM-Deepspeed-LoRA/data/test.json', type=str, help='')
+    # parser.add_argument('--train_path', default='/app/ChatGLM-Deepspeed-LoRA/data/alpaca_gpt4_data_zh_zx.json', type=str, help='')
+    parser.add_argument('--model_dir', default="/root/autodl-tmp/chatglm-6b", type=str, help='')
+    parser.add_argument('--num_train_epochs', default=1, type=int, help='')
+    parser.add_argument('--train_batch_size', default=2, type=int, help='')
+    parser.add_argument('--gradient_accumulation_steps', default=1, type=int, help='')
+    parser.add_argument('--output_dir', default='/app/ChatGLM-Deepspeed-LoRA/output_dir_lora/0503-speed', type=str, help='')
+    # parser.add_argument('--log_steps', type=int, default=10, help='')
+    parser.add_argument('--max_seq_length', type=int, default=512, help='')
+    parser.add_argument('--local_rank', type=int, default=0, help='')
+    parser.add_argument('--lora_r', type=int, default=8, help='')
+
+
     parser = deepspeed.add_config_arguments(parser)
     args = parser.parse_args()
 
@@ -202,20 +222,30 @@ def main():
 
     torch.distributed.barrier()
 
-    tokenizer = load_hf_tokenizer(args.model_name_or_path, fast_tokenizer=True)
-    tokenizer.pad_token = tokenizer.eos_token
+    model = ChatGLMForConditionalGeneration.from_pretrained(args.model_dir)
+    tokenizer = ChatGLMTokenizer.from_pretrained(args.model_dir)
+    config = transformers.AutoConfig.from_pretrained(args.model_dir,trust_remote_code=True)
 
-    model = create_hf_model(AutoModelForCausalLM,
-                            args.model_name_or_path,
-                            tokenizer,
-                            ds_config,
-                            disable_dropout=args.disable_dropout)
+    # tokenizer = load_hf_tokenizer(args.model_name_or_path, fast_tokenizer=True)
+    # tokenizer.pad_token = tokenizer.eos_token
 
-    if args.lora_dim > 0:
-        model = convert_linear_layer_to_lora(model, args.lora_module_name,
-                                             args.lora_dim)
-        if args.only_optimize_lora:
-            model = only_optimize_lora_parameters(model)
+    # model = create_hf_model(AutoModelForCausalLM,
+    #                         args.model_name_or_path,
+    #                         tokenizer,
+    #                         ds_config,
+    #                         disable_dropout=args.disable_dropout)
+
+    Lora_config = LoraConfig(r=args.lora_r,
+                        lora_alpha=32,
+                        target_modules=["query_key_value"],
+                        lora_dropout=0.1,
+                        bias="none",
+                        task_type="CAUSAL_LM",
+                        inference_mode=False,
+                        )
+
+    model = get_peft_model(model, Lora_config)
+    model = model.cuda()
 
     # Prepare the data
     train_phase = 1
@@ -230,6 +260,9 @@ def main():
         args.max_seq_len,
         sft_only_data_path=args.sft_only_data_path)
 
+
+    train_dataset = SFTDataSet(args.train_path, tokenizer, config, args.max_seq_length)
+    
     # DataLoaders creation:
     if args.local_rank == -1:
         train_sampler = RandomSampler(train_dataset)
@@ -238,7 +271,7 @@ def main():
         train_sampler = DistributedSampler(train_dataset)
         # eval_sampler = DistributedSampler(eval_dataset)
     train_dataloader = DataLoader(train_dataset,
-                                  collate_fn=default_data_collator,
+                                  collate_fn=coll_fn,
                                   sampler=train_sampler,
                                   batch_size=args.per_device_train_batch_size)
     # eval_dataloader = DataLoader(eval_dataset,
@@ -278,6 +311,7 @@ def main():
 
     num_update_steps_per_epoch = math.ceil(
         len(train_dataloader) / args.gradient_accumulation_steps)
+    
     lr_scheduler = get_scheduler(
         name=args.lr_scheduler_type,
         optimizer=optimizer,
@@ -326,17 +360,22 @@ def main():
 
     if args.output_dir is not None:
         print_rank_0('saving the final model ...', args.global_rank)
-        model = convert_lora_to_linear_layer(model)
+        # model = convert_lora_to_linear_layer(model)
 
-        if args.global_rank == 0:
-            save_hf_format(model, tokenizer, args)
+        # if args.global_rank == 0:
+        #     save_hf_format(model, tokenizer, args)
 
-        if args.zero_stage == 3:
-            # For zero stage 3, each gpu only has a part of the model, so we need a special save function
-            save_zero_three_model(model,
-                                  args.global_rank,
-                                  args.output_dir,
-                                  zero_stage=args.zero_stage)
+        # if args.zero_stage == 3:
+        #     # For zero stage 3, each gpu only has a part of the model, so we need a special save function
+        #     save_zero_three_model(model,
+        #                           args.global_rank,
+        #                           args.output_dir,
+        #                           zero_stage=args.zero_stage)
+        save_dir = os.path.join(args.output_dir, f"global_step-{global_step}")
+        model_engine.save_pretrained(save_dir, overwrite_output_dir=True)
+        copy(os.path.join(args.model_dir, "tokenizer_config.json"), os.path.join(save_dir, "tokenizer_config.json"))
+        copy(os.path.join(args.model_dir, "ice_text.model"), os.path.join(save_dir, "ice_text.model"))
+
 
 
 if __name__ == "__main__":
