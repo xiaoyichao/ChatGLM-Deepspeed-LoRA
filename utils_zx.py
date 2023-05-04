1dda1109b1a2c8c20f8caefd763ef1ce22be4871
xiaoyichao <xiao_yi_chao@163.com>
2023-05-04 21:22:30 +0800

    add checl

--- utils_zx.py
+++ utils_zx.py
@@ -8,8 +8,9 @@ data_path = "data/knowledge_zx.json"
 with open(txt_path,"r") as f:
     json_list = []
     lines = f.readlines()
+    print('数据长度',len(lines))
     num = 0
-    for line in lines:
+    for i, line in enumerate(lines):
         line = line.strip()
         line = json.loads(line)
         title = line["title"]
@@ -24,19 +25,22 @@ with open(txt_path,"r") as f:
                     Instruction = Instruction.strip()
                     print("请输入Answer，以 Ctrl-D 结束输入：")
                     Answer = sys.stdin.read()
-                    if Instruction != Instruction:
-                        dict_data = {
-                                "instruction": Instruction,
-                                "input": "",
-                                "output": Answer
-                                }
-                        json_list.append(dict_data)
-                        num+=1
+                    if Instruction != Answer:
+                        check = input("确定要这个吗?:1或者0")
+                        if check =="1":
+                            dict_data = {
+                                    "instruction": Instruction,
+                                    "input": "",
+                                    "output": Answer
+                                    }
+                            json_list.append(dict_data)
+                            num+=1    
+
                     else:
                         print("这条数据不要了。")
                 os.system('cls' if os.name == 'nt' else 'clear')
-                print("可用数据：", num)
-                if num>=10:
+                print("可用数据：", num, "当前索引", i)
+                if num>=100:
                     break
 
 
